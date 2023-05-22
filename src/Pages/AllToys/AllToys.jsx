c3633de3d6ed47c37a6ff145507af1302ee023d9
Mizanur Rahman <mizanrxb@gmail.com>
2023-05-22 14:34:43 +0600

    search functionality added

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -4,6 +4,7 @@ import ToyList from "./toyList";
 
 const AllToys = () => {
   const [toys, setToys] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/alltoys")
@@ -11,11 +12,33 @@ const AllToys = () => {
       .then((data) => setToys(data));
   }, []);
 
+  const handleSearch = () => {
+    fetch(`http://localhost:5000/getToyByName/${searchText}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setToys(data);
+      });
+  };
+
+
+
+
   return (
     <div>
-      <h1 className="mb-4 text-3xl font-medium text-center text-red-500">
+      <h1 className="mx-auto mb-4 text-3xl font-medium text-center text-red-500">
         All Toys
       </h1>
+      <div className="w-2/5 p-4 mx-auto text-center">
+          <input
+            onChange={(e) => setSearchText(e.target.value)}
+            type="text"
+            placeholder="Search By Toy Name"
+            className="w-2/4 p-3 mr-6 text-sm border-b-2 border-gray-100 rounded shadow-md focus-visible:outline-none focus:border-red-500 pe-12"
+          />{" "}
+          <button className="btn-primary" onClick={handleSearch}>Search</button>
+        </div>
+
 
       <div className="flex flex-col my-container">
         <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
